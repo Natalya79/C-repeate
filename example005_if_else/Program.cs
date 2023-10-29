71645ea85b9f85346cf73e5cee465d011f0eacae
Natalia <raisonn.ng@gmail.com>
2023-10-29 11:26:02 +0300

    add condition

---
+++ example005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите Ваше имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша") // позволяет включать все регистры
+{
+    Console.WriteLine("Ура, это Маша!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
